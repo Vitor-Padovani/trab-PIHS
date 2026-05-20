ecb1bcca78d916ad6370823df596d96ebcba445c
Vitor Padovani <vitor.azpadovani@gmail.com>
2026-05-20 09:02:49 -0300

    add multiplication, division, power and squareroot

--- calculator.s
+++ calculator.s
@@ -2,13 +2,18 @@
 
 .section .data
 	msg_op1:	.asciz "Type the first operand:\n"
-	msg_opt:	.asciz "Type the operator (+, -):\n"
+	msg_opt:	.asciz "Type the operator (+, -, *, /, ^, r):\n"
 	msg_op2:	.asciz "Type the second operand:\n"
 	msg_cont:	.asciz "Continue? (y/n)"
 
 	fmt_opn_in:		.asciz "%lf"
 	fmt_char_in:	.asciz " %c"
 	fmt_result: 	.asciz "-> %g\n"
+
+	msg_div_zero:	.asciz "Error: Division by zero not allowed\n"
+    msg_neg_sqrt:   .asciz  "Error: Negative squre root not allowed\n"
+
+    const_one:      .double 1.0
 
 .section .bss
 	.comm	op1,	8
@@ -33,7 +38,82 @@ op_subtraction:
     pop %rbp
     ret
 
-// TODO add more
+op_mult:
+    push %rbp
+    mov %rsp, %rbp
+    mulsd %xmm1, %xmm0
+	mov %rbp, %rsp
+    pop %rbp
+    ret
+
+op_div:
+    push %rbp
+    mov %rsp, %rbp
+
+	// Verifies division by zero
+    xorpd %xmm2, %xmm2
+    ucomisd %xmm2, %xmm1
+    jne .div_allowed
+
+    lea msg_div_zero(%rip), %rdi
+    xor %rax, %rax
+    call printf
+    xor %rax, %rax
+    jmp .div_finish
+
+	.div_allowed:
+		divsd %xmm1, %xmm0
+		mov $1, %rax
+
+	.div_finish:
+		mov %rbp, %rsp
+		pop %rbp
+		ret
+
+op_pow:
+    push %rbp
+    mov %rsp, %rbp
+
+    # Converts exponent to integer
+    cvttsd2si %xmm1, %rcx
+
+    movsd const_one(%rip), %xmm1
+    test %rcx, %rcx
+    jz .pow_finish
+
+.pow_loop:
+    mulsd %xmm0, %xmm1
+    dec %rcx
+    jnz .pow_loop
+
+.pow_finish:
+    movsd %xmm1, %xmm0
+
+    pop %rbp
+    ret
+
+op_sqrt:
+    push %rbp
+    mov %rsp, %rbp
+
+	// Verifies negative square root
+    xorpd %xmm1, %xmm1
+    ucomisd %xmm0, %xmm1
+    jbe .sqrt_allowed
+
+    lea msg_neg_sqrt(%rip), %rdi
+    xor %rax, %rax
+    call printf
+    xor %rax, %rax
+    jmp .sqrt_finish
+
+	.sqrt_allowed:
+		sqrtsd  %xmm0, %xmm0
+		mov     $1, %rax
+
+	.sqrt_finish:
+		pop     %rbp
+		ret
 
 // ========== MAIN FUNCTIONS ========== //
 show_result:
@@ -95,23 +175,14 @@ main:
 	je .case_sum
 	cmpb $'-', %al
 	je .case_subtraction
-
-// ========== CASES ========== //
-.case_sum:
-	call read_op2
-    movsd op1(%rip), %xmm0
-    movsd op2(%rip), %xmm1
-    call op_sum
-    call show_result
-    jmp .main_repeat
-
-.case_subtraction:
-	call read_op2
-    movsd op1(%rip), %xmm0
-    movsd op2(%rip), %xmm1
-    call op_subtraction
-    call show_result
-    jmp .main_repeat
+	cmpb $'*', %al
+	je .case_multplication
+	cmpb $'/', %al
+	je .case_division
+	cmpb $'^', %al
+	je .case_pow
+	cmpb $'r', %al
+	je .case_sqrt
 
 # Asks if the user wants to repeat or finish
 .main_repeat:
@@ -132,3 +203,53 @@ main:
 	xor %rax, %rax
 	ret
 
+// ========== CASES ========== //
+.case_sum:
+	call read_op2
+    movsd op1(%rip), %xmm0
+    movsd op2(%rip), %xmm1
+    call op_sum
+    call show_result
+    jmp .main_repeat
+
+.case_subtraction:
+	call read_op2
+    movsd op1(%rip), %xmm0
+    movsd op2(%rip), %xmm1
+    call op_subtraction
+    call show_result
+    jmp .main_repeat
+
+.case_multplication:
+    call read_op2
+    movsd op1(%rip), %xmm0
+    movsd op2(%rip), %xmm1
+    call op_mult
+    call show_result
+    jmp .main_repeat
+
+.case_division:
+    call read_op2
+    movsd op1(%rip), %xmm0
+    movsd op2(%rip), %xmm1
+    call op_div
+    test %rax, %rax
+    jz .main_repeat
+    call show_result
+    jmp .main_repeat
+
+.case_pow:
+    call    read_op2
+    movsd   op1(%rip), %xmm0
+    movsd   op2(%rip), %xmm1
+    call    op_pow
+    call    show_result
+    jmp     .main_repeat
+
+.case_sqrt:
+    movsd   op1(%rip), %xmm0
+    call    op_sqrt
+    test    %rax, %rax
+    jz      .main_repeat
+    call    show_result
+    jmp     .main_repeat
